76ca42c58897637ca30a44b633f51c0d32688db0
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2022-10-15 14:43:31 -0600

    Correct variable string names  add integer variables

--- variables.rb
+++ variables.rb
@@ -1,6 +1,9 @@
-code_name = "Tesseract"
+player_name = "Tesseract"
 combat_class = "Techno-Mage"
-weapon_class = "Server Staff"
-sheild_class = "Terminal Tome"
-
+class_level = 1
+weapon_type = "Lv.1 Server Staff"
+sheild_type = "Lv.1 Terminal Tome"
+electro-mana_total = 1000
+hitpoint_total = 500
+spell_library_capacity = 4
 
