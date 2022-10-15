01654691808f01ac80ade77dd67204c6fdbfe25c
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2022-10-15 15:12:59 -0600

    Reassigns player gear stats and outputs player's basic information

--- variables.rb
+++ variables.rb
@@ -4,7 +4,7 @@ class_level = 1
 weapon_type = "Lv.1 Server Staff"
 sheild_type = "Lv.1 Terminal Tome"
 familiar = "Rock Crab"
-electro-mana_total = 1000
+electromana_total = 1000
 hitpoint_total = 500
 spell_library_capacity = 4
 hours_played = 1.5
@@ -15,3 +15,12 @@ completed_tutorial = true
 started_main_quest_line = false
 member_of_a_guild = false
 familiar_acqired = true
+weapon_type = "Lv.2 Server Staff"
+sheild_type = "Lv.2 Terminal Tome"
+familiar_type = "Armored Mouse"
+puts player_name
+puts combat_class
+puts class_level
+puts hitpoint_total
+puts electromana_total
+puts spell_library_capacity
